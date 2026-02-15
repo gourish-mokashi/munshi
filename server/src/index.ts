496cced15585e716e41bea0419d05c67a646f419
Shreehari-Acharya <shreehari.acharya.06@gmail.com>
2026-02-15 14:51:39 +0530

    fix use of middleware in protected routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,12 +7,14 @@ import stockRouter from "./routes/stockRoutes.js";
 import analyticsRouter from "./routes/analyticsRoutes.js";
 import transactionRouter from "./routes/transactionRoutes.js";
 import aiRouter from "./routes/aiRoutes.js";
+import { requireAuth } from "./middlewares/authMiddleware.js";
 
 const app: express.Application = express();
 
 app.all("/api/auth/{*any}", toNodeHandler(auth)); 
 app.use(express.json());
 
+app.use(requireAuth); 
 app.use("/products", productRouter);
 app.use("/stock", stockRouter);
 app.use("/analytics", analyticsRouter);
